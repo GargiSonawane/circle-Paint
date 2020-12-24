077f08794788662012f9f6f2df3fafae40adb1f9
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2020-12-24 12:26:20 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,61 @@
-canvas = document.getElementById('myCanvas');
-ctx= canvas.getContext('2d');
+canvas= document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+color = "black";
+width = 1;
+radius = 10;
+var mouseEvent="empty";
+var current_x;
+var current_y;
+canvas.addEventListener("mousedown",myMouseDown);
+function myMouseDown(e){
+    color=document.getElementById("color").value;
+    width=document.getElementById("width").value;
+    radius=document.getElementById("radius").value;
+    mouseEvent="mouseDown";
+}
+ canvas.addEventListener("mouseup",myMouseUp);
+ function myMouseUp(e){
+     mouseEvent="mouseup";
+ }
+ canvas.addEventListener("mouseleave",myMouseLeave);
+ function myMouseLeave(e){
+     mouseEvent="mouseleave";
+ }
+ canvas.addEventListener("mousemove",myMouseMove);
+ function myMouseMove(e){
+     currentMouse_x=e.clientX-canvas.offsetLeft;
+     currentMouse_y=e.clientY-canvas.offsetTop;
 
-ctx.beginPath();
-ctx.strokeStyle = "grey";
-ctx.lineWidth = 1;
-ctx.rect(150,143,430,200);
-ctx.stroke();
+     if(mouseEvent == "mouseDown"){
+         ctx.beginPath();
+         ctx.strokeStyle = color;
+         ctx.lineWidth= width;
+         ctx.arc(current_x, current_y, radius, 0, 2*Math.PI);
+         ctx.stroke();
+     }
+     lastPositionOfX=current_x;
+     lastPositionOfY=current_y;
+ }
+ canvas.addEventListener("touchstart",myTouchStart);
+ function myTouchStart(){
+     lastPositionOfTouchX=current_x;
+     lastPositionOfTouchY=current_y;
+ }
+ canvas.addEventListener("touchmove", my_touchmove);
+    function my_touchmove(e)
+    {
+         current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+         current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width;
+        ctx.moveTo(lastPositionOfTouchX,lastPositionOfTouchY);
+        ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+        ctx.stroke();
 
-ctx.beginPath();
-ctx.strokeStyle = "blue";
-ctx.lineWidth = 5;
-ctx.arc(250 ,210 ,40 ,0 ,2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "black";
-ctx.lineWidth = 5;
-ctx.arc(345 ,210 ,40 ,0 ,2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.lineWidth = 5;
-ctx.arc(445 ,210 ,40 ,0 ,2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "green";
-ctx.lineWidth = 5;
-ctx.arc(400 ,250 ,40 ,0 ,2 * Math.PI);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.strokeStyle = "yellow";
-ctx.lineWidth = 5;
-ctx.arc(295 ,250 ,40 ,0 ,2 * Math.PI);
-ctx.stroke();
+        lastPositionOfTouchX = current_position_of_touch_x; 
+        lastPositionOfTouchY = current_position_of_touch_y;
+    }
+ function clearArea(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+}
