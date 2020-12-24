41ac8d0c5f7536c0408b7792309a8184ad2b4949
Gargi Sonawane <67555203+GargiSonawane@users.noreply.github.com>
2020-12-24 12:23:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
-var mouseEvent="empty";
-var lastPositionOfX;
-var lastPositionOfY;
-canvas= document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
-color = "black";
-width = 1;
-radius = 10;
-canvas.addEventListener("mousedown",myMouseDown);
-function myMouseDown(e){
-    color=document.getElementById("color").value;
-    width=document.getElementById("width").value;
-    radius=document.getElementById("radius").value;
-    mouseEvent="mouseDown";
-}
- canvas.addEventListener("mouseup",myMouseUp);
- function myMouseUp(e){
-     mouseEvent="mouseup";
- }
- canvas.addEventListener("mouseleave",myMouseLeave);
- function myMouseLeave(e){
-     mouseEvent="mouseleave";
- }
- canvas.addEventListener("mousemove",myMouseMove);
- function myMouseMove(e){
-     current_x=e.clientX-canvas.offsetLeft;
-     current_y=e.clientY-canvas.offsetTop;
-     if(mouseEvent == "mouseDown"){
-         console.log(current_x + ", " + 
-         current_y);
-         ctx.beginPath();
-         ctx.strokeStyle = color;
-         ctx.lineWidth= width;
-         ctx.arc(current_x, current_y, radius, 0, 2*Math.PI);
-         ctx.stroke();
-     }
-     lastPositionOfX=current_x;
-     lastPositionOfY=current_y;
- }
- function clearArea(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-}
+canvas = document.getElementById('myCanvas');
+ctx= canvas.getContext('2d');
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth = 1;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(250 ,210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(345 ,210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(445 ,210 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(400 ,250 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(295 ,250 ,40 ,0 ,2 * Math.PI);
+ctx.stroke();
